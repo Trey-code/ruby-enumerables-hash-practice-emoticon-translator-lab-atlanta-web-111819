6516f4c7443452b1d9cf56aeff93861ddf2acb90
Treven Womack <keytonwomack@yahoo.com>
2019-11-18 11:38:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,10 @@ def get_japanese_emoticon(path, emoticon)
 end
 
 def get_english_meaning(path, emoticon)
-
+emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil 
+    result = " Sorry, that emoticon was not found"
+  end
+  result
 end
