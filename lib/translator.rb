c63bb9289c8f42220f7e93cf32f3f76b0398f666
Treven Womack <keytonwomack@yahoo.com>
2019-11-18 11:38:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,13 +9,20 @@ emoticon_hash["get_emoticon"] = Hash.new
 emoticon_hash["get_meaning"] =Hash.new
 
 emoticons.each do |english_word, emoticon_set|
-emoticon_hash["get_emoticon"][emoticon_set.first]=em
+emoticon_hash["get_emoticon"][emoticon_set.first]=emoticon_set.last 
+emoticon_hash["get_meaning"][emoticon_set.last]= english_word
+end
+emoticon_hash 
+end
+def get_japanese_emoticon(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil 
+    result = " Sorry, that emoticon was not found"
+  end
+  result
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
 end
